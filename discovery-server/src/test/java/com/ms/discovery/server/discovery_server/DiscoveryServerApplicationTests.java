597030166674b312cb80d1750de4844b0c802c7d
P2491Dev <pradeep.maurya.dev@gmail.com>
2025-09-06 14:55:47 +0530

    Eureka server

---
+++ discovery-server/src/test/java/com/ms/discovery/server/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,10 @@
+package com.ms.discovery.server.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+
+}
